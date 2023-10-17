9ef7b8597962870405ac6e6b81d91fd72ba8974f
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-10-17 12:09:47 +0900

    feat: 에디터 자동저장 디바운싱 적용

--- src/main.js
+++ src/main.js
@@ -7,14 +7,18 @@ const $target = document.querySelector("#app");
 const TEMP_POST_SAVE_KEY = "temp-post";
 const post = getItem(TEMP_POST_SAVE_KEY, { title: "", content: "" });
 
+let timer = null;
 new Editor({
   $target,
   initialState: post,
   onEditing: (post) => {
-    setItem("temp-post", {
-      ...post,
-      tempSaveDate: new Date(),
-    });
+    if (timer !== null) clearTimeout(timer);
+    timer = setTimeout(() => {
+      setItem("temp-post", {
+        ...post,
+        tempSaveDate: new Date(),
+      });
+    }, 500);
   },
 });
 
