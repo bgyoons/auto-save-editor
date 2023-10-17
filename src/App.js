73e9b3810843e4ba90d601ca97a993cd7ba9ec68
Baeg <73841260+bgyoons@users.noreply.github.com>
2023-10-17 13:24:58 +0900

    feat: pathname 통한 라우팅 처리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import PostEditPage from "./PostEditPage.js";
 import PostsPage from "./PostsPage.js";
 
 export default function App({ $target }) {
   const postsPage = new PostsPage({ $target });
-  postsPage.render();
+  const postEditPage = new PostEditPage({
+    $target,
+    initialState: {
+      postId: "new",
+    },
+  });
+
+  this.route = () => {
+    const { pathname } = window.location;
+    if (pathname === "/") postsPage.render();
+    else if (pathname.indexOf("/posts/") === 0) {
+      const [, , postId] = pathname.split("/");
+      postEditPage.setState({ postId });
+    }
+  };
+
+  this.route();
 }
--- src/main.js
+++ src/main.js
@@ -1,15 +1,5 @@
 import App from "./App.js";
-import PostEditPage from "./PostEditPage.js";
 
 const $target = document.querySelector("#app");
 
-const postEditPage = new PostEditPage({
-  $target,
-  initialState: {
-    postId: "new",
-  },
-});
-postEditPage.setState({
-  postId: 1,
-});
-// new App({ $target });
+new App({ $target });
